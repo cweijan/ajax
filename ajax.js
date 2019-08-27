763b0ecc204f12ae40784b9459a42e7fdd549aa4
cweijan <cweijan@163.com>
2019-08-27 16:59:11 +0800

    增加Promise支持, 不支持Promise的浏览器将无法使用!

--- ajax.js
+++ ajax.js
@@ -2,6 +2,30 @@
 
     function ajax(options) {
 
+        var url = options.url || "" //请求的链接
+        var method = ((options.type || options.method) || "get").toLowerCase(); //请求的方法,默认为get
+        var data = options.data || null; //请求的数据
+        var contentType = options.contentType || ""; //请求头
+        var dataType = options.dataType || ""; //请求的类型
+        var async = options.async === undefined ? true : options.async; //是否异步，默认为true.
+        var timeOut = options.timeOut; //超时时间。 
+        var before = options.before || function () { }; //发送之前执行的函数
+        var error = options.error || function () { }; //错误执行的函数
+        var success = options.success || function () { }; //请求成功的回调函数
+        var complete = options.complete || function () { }; //请求结束的回调函数
+        var timeout_bool = false; //是否请求超时
+        var timeout_flag = null; //超时标识
+        var xhr = null; //xhr对角
+        setData();
+        if (dataType === "jsonp") {
+            return createJsonp();
+        } else {
+            return createXHR();
+        }
+
+        /**
+         * 将object转为FormData
+         */
         function convertToFormData() {
             var formData = new FormData()
             for (var key in data) {
@@ -19,6 +43,9 @@
             return false;
         }
 
+        /**
+         * 如果数据是FormData,将其转为object
+         */
         function processFormData() {
             if (data instanceof FormData) {
                 var temp = {};
@@ -29,53 +56,62 @@
             }
         }
 
+        /**
+         * 将对象转为name=value形式
+         * @param {*} name 
+         * @param {*} value 
+         * @param {*} parentName 处理数组用的
+         */
+        function encodeData(name, value, parentName) {
+            var items = [];
+            name = parentName === undefined ? name : parentName + "[" + name + "]";
+            if (typeof value === "object" && value !== null) {
+                items = items.concat(setObjData(value, name));
+            } else {
+                name = encodeURIComponent(name);
+                value = encodeURIComponent(value);
+                if (value != "null" && value != "undefined" && value) items.push(name + "=" + value);
+            }
+            return items;
+        }
+
+
+        function setObjData(data, parentName) {
+
+            var arr = [], value;
+            if (Object.prototype.toString.call(data) == '[object Array]') {
+                for (var i = 0, len = data.length; i < len; i++) {
+                    value = data[i];
+                    arr = arr.concat(encodeData(typeof value == "object" ? i : "", value, parentName));
+                }
+            } else if (Object.prototype.toString.call(data) == '[object Date]') {
+                console.warn("found date type:" + parentName + ",do not use!")
+                arr = arr.concat(encodeData(parentName, data.toDateString()));
+            } else if (Object.prototype.toString.call(data) == '[object Object]') {
+                for (var key in data) {
+                    value = data[key];
+                    arr = arr.concat(encodeData(key, value, parentName));
+                }
+            }
+            return arr;
+        }
+
+        //设置字符串的遍码，字符串的格式为：a=1&b=2;
+        function setStrData(data) {
+            var arr = data.split("&");
+            for (var i = 0, len = arr.length; i < len; i++) {
+                var name = encodeURIComponent(arr[i].split("=")[0]);
+                var value = encodeURIComponent(arr[i].split("=")[1]);
+                if (value != "null" && value != "undefined" && value) arr[i] = name + "=" + value;
+            }
+            return arr;
+        }
+
         //编码数据
         function setData() {
             if (typeof data === "function") {
                 data = data()
             }
-
-            //设置对象的遍码
-            function setObjData(data, parentName) {
-                function encodeData(name, value, parentName) {
-                    var items = [];
-                    name = parentName === undefined ? name : parentName + "[" + name + "]";
-                    if (typeof value === "object" && value !== null) {
-                        items = items.concat(setObjData(value, name));
-                    } else {
-                        name = encodeURIComponent(name);
-                        value = encodeURIComponent(value);
-                        if (value != "null" && value != "undefined" && value) items.push(name + "=" + value);
-                    }
-                    return items;
-                }
-
-                var arr = [], value;
-                if (Object.prototype.toString.call(data) == '[object Array]') {
-                    for (var i = 0, len = data.length; i < len; i++) {
-                        value = data[i];
-                        arr = arr.concat(encodeData(typeof value == "object" ? i : "", value, parentName));
-                    }
-                } else if (Object.prototype.toString.call(data) == '[object Object]') {
-                    for (var key in data) {
-                        value = data[key];
-                        arr = arr.concat(encodeData(key, value, parentName));
-                    }
-                }
-                return arr;
-            }
-
-            //设置字符串的遍码，字符串的格式为：a=1&b=2;
-            function setStrData(data) {
-                var arr = data.split("&");
-                for (var i = 0, len = arr.length; i < len; i++) {
-                    var name = encodeURIComponent(arr[i].split("=")[0]);
-                    var value = encodeURIComponent(arr[i].split("=")[1]);
-                    if (value != "null" && value != "undefined" && value) arr[i] = name + "=" + value;
-                }
-                return arr;
-            }
-
 
             if (/\bjson\b/.test(contentType) || method === "json") {
                 processFormData();
@@ -105,20 +141,22 @@
 
         // JSONP
         function createJsonp() {
-            before();
-            var script = document.createElement("script"),
-                timeName = new Date().getTime() + Math.round(Math.random() * 1000),
-                callback = "JSONP_" + timeName;
 
-            window[callback] = function (data) {
-                clearTimeout(timeout_flag);
-                document.body.removeChild(script);
-                success(data);
-            }
+            var script = document.createElement("script");
+            var callback = "JSONP_" + new Date().getTime() + Math.round(Math.random() * 1000);
             script.src = url + (url.indexOf("?") > -1 ? "&" : "?") + "callback=" + callback;
             script.type = "text/javascript";
+            before();
             document.body.appendChild(script);
             setTime(callback, script);
+            return new Promise((resolve, reject) => {
+                window[callback] = function (data) {
+                    clearTimeout(timeout_flag);
+                    document.body.removeChild(script);
+                    success(data);
+                    resolve(data)
+                }
+            })
         }
 
         //设置请求超时
@@ -193,58 +231,36 @@
             } else if (contentType) {
                 xhr.setRequestHeader("Content-Type", contentType);
             }
-            //添加监听
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4) {
-                    xhr = xhrWrap(xhr)
-                    if (timeOut !== undefined) {
-                        //由于执行abort()方法后，有可能触发onreadystatechange事件，
-                        //所以设置一个timeout_bool标识，来忽略中止触发的事件。
-                        if (timeout_bool) {
-                            return;
-                        }
-                        clearTimeout(timeout_flag);
-                    }
-                    if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-                        success(xhr.response);
-                    } else {
-                        error(xhr);
-                    }
-                    complete(xhr)
-                }
-            };
-            //发送请求
-            let sendData = method === "get" ? null : data;
+
+            var sendData = method === "get" ? null : data;
             before(sendData);
-            xhr.send(sendData);
-            setTime(); //请求超时
-        }
+            return new Promise((resolve, reject) => {
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        xhr = xhrWrap(xhr)
+                        if (timeOut !== undefined) {
+                            //由于执行abort()方法后，有可能触发onreadystatechange事件，
+                            //所以设置一个timeout_bool标识，来忽略中止触发的事件。
+                            if (timeout_bool) {
+                                return;
+                            }
+                            clearTimeout(timeout_flag);
+                        }
 
-
-        var url = options.url || "", //请求的链接
-            method = ((options.type || options.method) || "get").toLowerCase(), //请求的方法,默认为get
-            data = options.data || null, //请求的数据
-            contentType = options.contentType || "", //请求头
-            dataType = options.dataType || "", //请求的类型
-            async = options.async === undefined ? true : options.async, //是否异步，默认为true.
-            timeOut = options.timeOut, //超时时间。 
-            before = options.before || function () {
-            }, //发送之前执行的函数
-            error = options.error || function () {
-            }, //错误执行的函数
-            success = options.success || function () {
-            }, //请求成功的回调函数
-            complete = options.complete || function () {
-            }; //请求结束的回调函数
-        var timeout_bool = false, //是否请求超时
-            timeout_flag = null, //超时标识
-            xhr = null; //xhr对角
-        setData();
-
-        if (dataType === "jsonp") {
-            createJsonp();
-        } else {
-            createXHR();
+                        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+                            success(xhr.response);
+                            complete(xhr)
+                            resolve(xhr.response)
+                        } else {
+                            error(xhr);
+                            complete(xhr)
+                            reject(xhr)
+                        }
+                    }
+                };
+                xhr.send(sendData);
+                setTime(); //请求超时
+            })
         }
     }
 
@@ -254,7 +270,7 @@
             callback = callback ? callback : data
             data = null
         }
-        ajax({
+        return ajax({
             url: url, method: 'POST', data: data, success: callback
         })
     }
@@ -263,7 +279,7 @@
             callback = callback ? callback : data
             data = null
         }
-        ajax({
+        return ajax({
             url: url, method: 'get', data: data, success: callback
         })
     }
@@ -272,7 +288,7 @@
             callback = callback ? callback : data
             data = null
         }
-        ajax({
+        return ajax({
             url: url, method: 'POST', data: data, success: callback, contentType: "application/json; charset=utf-8"
         })
     }
